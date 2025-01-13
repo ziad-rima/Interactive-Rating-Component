0ab07c405058e36ea04bdde3a8128fab80e9e6d5
Ziad Rima <ziadrima49@gmail.com>
2025-01-13 17:09:41 +0100

    add and style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-
+import Content from "./components/Content"
+import Ratings from "./components/Ratings"
+import SubmitButton from "./components/SubmitButton"
+import SecondPage from "./components/SecondPage"
 const App = () => {
   return (
-    <main className="main-component">
-      <Header />
+    <>
+      <main className="main-component">
+        <Header />
+        <Content />
+        <Ratings />
+        <SubmitButton />
+      </main>
+      <div className="thankyou-component">
+        <SecondPage />
+      </div>
       <Footer />
-    </main>
+    </>
+    
   )
 }
 

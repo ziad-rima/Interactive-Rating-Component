9bdb62d0fcba3b76811ab0d399a00313ab606d37
Ziad Rima <ziadrima49@gmail.com>
2025-01-13 17:12:39 +0100

    add submit button component

---
+++ src/components/SubmitButton.jsx
@@ -0,0 +1,9 @@
+const SubmitButton = () => {
+  return (
+    <div className="submit-button-container">
+      <button type="submit" className="submit-button" id="submit-button">SUBMIT</button>
+    </div>
+  )
+}
+
+export default SubmitButton

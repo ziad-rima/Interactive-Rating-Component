45e93cfff43c1d94393215ef988cbc2692240f20
Ziad Rima <ziadrima49@gmail.com>
2025-01-13 17:11:28 +0100

    add Content.jsx component

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+const Content = () => {
+  return (
+    <div className="text-container">
+      <p className="text-content">
+        Please let us know how we did with your support request. All feedback is appreciated 
+        to help us improve our offering!
+      </p>
+    </div>
+  )
+}
+
+export default Content

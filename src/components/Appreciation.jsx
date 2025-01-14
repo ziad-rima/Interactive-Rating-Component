604163d1e92ad91f4f8824a6aad9471f1b603683
Ziad Rima <ziadrima49@gmail.com>
2025-01-14 11:53:47 +0100

    create Appreciation.jsx component

---
+++ src/components/Appreciation.jsx
@@ -0,0 +1,11 @@
+const Appreciation = () => {
+  return (
+    <div className="appreciation-section">
+      <h1 className="appreciation-title">Thank you!</h1>
+      <p className="appreciation-message">We appreciate you taking the time to give a rating. If you ever need more support, 
+      don’t hesitate to get in touch!</p>
+    </div>
+  )
+}
+
+export default Appreciation
